f515df60bb392034db647a884f2b26b6c2ebe3ad
RafaMC03 <rafamorais0810@gmail.com>
2022-10-21 23:28:15 -0300

    second commit

---
+++ lib/modules/index/pages/index.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:tasker/modules/cadastro/pages/cadastro.dart';
+import 'package:tasker/modules/login/pages/login.dart';
+import 'package:tasker/shared/components/botoes/botao_componente.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                stops: [
+              0.1,
+              1
+            ],
+                colors: [
+              Color.fromARGB(255, 12, 175, 158),
+              Color.fromARGB(255, 22, 122, 154),
+            ])),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: const [
+                     Text(
+                      'TASKER',
+                      style: TextStyle(
+                          fontSize: 80,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          height: 0.4),
+                    ),
+                    Text(
+                      'Registro de Tarefas',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 60),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      BotaoComponente(
+                            texto: 'Entrar', 
+                            corFundo: Colors.white, 
+                            onPressed: () {
+                              Navigator.push(context, MaterialPageRoute(builder: ((context) => const LoginPage())));
+                            }, 
+                            corTexto: const Color.fromARGB(255, 22, 122, 154),
+                            corSplash: const Color.fromARGB(255, 22, 122, 154),
+                            ),
+                          const Padding(
+                            padding: EdgeInsets.only(top: 13.0),
+                            child: Text('Não tem uma conta?',
+                            style: TextStyle(
+                              color: Colors.white
+                            ),
+                            ),
+                          ),
+                          BotaoComponente(
+                            texto: 'Cadastrar', 
+                            corFundo: const Color.fromARGB(255, 22, 122, 154), 
+                            onPressed: () {
+                              Navigator.push(context, MaterialPageRoute(builder: ((context) => const CadastroPage())));
+                            }, 
+                            corTexto: Colors.white,
+                            corBorda: Colors.white,
+                            ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/modules/login/pages/login.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:tasker/shared/components/botoes/botao_componente.dart';
+import 'package:tasker/shared/components/campo_form/campo_form_componente.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _controller1 = TextEditingController();
+  final _controller2 = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          decoration:
+              const BoxDecoration(color: Color.fromARGB(255, 241, 241, 241)),
+          child: Center(
+            child: Column(
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.only(top: 150.0),
+                      child: Text(
+                        'TASKER',
+                        style: TextStyle(
+                            fontSize: 100,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 12, 175, 158),
+                            height: 0.4),
+                      ),
+                    ),
+                    Text(
+                      'Registro de Tarefas',
+                      style: TextStyle(
+                          fontSize: 18, color: Color.fromARGB(255, 12, 175, 158)),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.1),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    height: MediaQuery.of(context).size.height * 0.48,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 25, 20, 25),
+                      child: Form(
+                        child: Column(
+                          children: [
+                            const FittedBox(
+                              fit: BoxFit.fill,
+                              child: Text(
+                                "Entre na sua conta",
+                                style: TextStyle(
+                                    wordSpacing: 4,
+                                    letterSpacing: 2,
+                                    fontSize: 24,
+                                    color: Color.fromARGB(255, 12, 175, 158),
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            const Padding(padding: EdgeInsets.all(10)),
+                            CampoForm(
+                              label: 'Email',
+                              controller: _controller1,
+                              icone: Icons.email, 
+                              senha: false,
+                            ),
+                            const Padding(padding: EdgeInsets.all(20)),
+                            CampoForm(
+                              label: 'Senha',
+                              controller: _controller2,
+                              icone: Icons.visibility_off, 
+                              senha: true,
+                            ),
+                            const Align(
+                                alignment: Alignment.topLeft,
+                                child: Text(
+                                  'Esqueceu a senha?',
+                                  style: TextStyle(color: Colors.grey),
+                                )),
+                            const Padding(padding: EdgeInsets.all(20)),
+                            BotaoComponente(
+                              texto: 'Entrar',
+                              corFundo: const Color.fromARGB(255, 12, 175, 158),
+                              onPressed: () {},
+                              corTexto: Colors.white,
+                              corSplash: Colors.white,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 40),
+                  child: Text.rich(TextSpan(
+                      text: 'Não tem uma conta? ',
+                      children: <InlineSpan>[
+                        TextSpan(
+                            text: 'Cadastre-se agora',
+                            style: const TextStyle(
+                                color: Color.fromARGB(255, 12, 175, 158),
+                                decoration: TextDecoration.underline),
+                            recognizer: TapGestureRecognizer()..onTap = () => Navigator.pop(context),
+                        )
+                      ])),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
