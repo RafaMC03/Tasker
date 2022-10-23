133f9335918ae23933682821bc72a54261b1e644
ichin23 <pedroferreiramanoel@gmail.com>
2022-10-23 09:05:26 -0300

    Fix commit

--- lib/modules/login/pages/login.dart
+++ lib/modules/login/pages/login.dart
@@ -17,7 +17,6 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-<<<<<<< HEAD
       body: SingleChildScrollView(
         child: Container(
           decoration:
@@ -42,7 +41,8 @@ class _LoginPageState extends State<LoginPage> {
                     Text(
                       'Registro de Tarefas',
                       style: TextStyle(
-                          fontSize: 18, color: Color.fromARGB(255, 12, 175, 158)),
+                          fontSize: 18,
+                          color: Color.fromARGB(255, 12, 175, 158)),
                     ),
                   ],
                 ),
@@ -77,15 +77,15 @@ class _LoginPageState extends State<LoginPage> {
                             CampoForm(
                               label: 'Email',
                               controller: _controller1,
-                              icone: Icons.email, 
-                              senha: false,
+                              icone: Icons.email,
+                              isSenha: false,
                             ),
                             const Padding(padding: EdgeInsets.all(20)),
                             CampoForm(
                               label: 'Senha',
                               controller: _controller2,
-                              icone: Icons.visibility_off, 
-                              senha: true,
+                              icone: Icons.visibility_off,
+                              isSenha: true,
                             ),
                             const Align(
                                 alignment: Alignment.topLeft,
@@ -103,130 +103,27 @@ class _LoginPageState extends State<LoginPage> {
                             ),
                           ],
                         ),
-=======
-      body: Container(
-        decoration:
-            const BoxDecoration(color: Color.fromARGB(255, 241, 241, 241)),
-        child: Center(
-          child: Column(
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Padding(
-                    padding: EdgeInsets.only(top: 150.0),
-                    child: Text(
-                      'TASKER',
-                      style: TextStyle(
-                          fontSize: 100,
-                          fontWeight: FontWeight.bold,
-                          color: Color.fromARGB(255, 12, 175, 158),
-                          height: 0.4),
-                    ),
-                  ),
-                  Text(
-                    'Registro de Tarefas',
-                    style: TextStyle(
-                        fontSize: 18, color: Color.fromARGB(255, 12, 175, 158)),
-                  ),
-                ],
-              ),
-              Padding(
-                padding: EdgeInsets.only(
-                    top: MediaQuery.of(context).size.height * 0.1),
-                child: Container(
-                  width: MediaQuery.of(context).size.width * 0.8,
-                  height: MediaQuery.of(context).size.height * 0.48,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(20, 25, 20, 25),
-                    child: Form(
-                      child: Column(
-                        children: [
-                          const FittedBox(
-                            fit: BoxFit.fill,
-                            child: Text(
-                              "Entre na sua conta",
-                              style: TextStyle(
-                                  wordSpacing: 4,
-                                  letterSpacing: 2,
-                                  fontSize: 24,
-                                  color: Color.fromARGB(255, 12, 175, 158),
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                          const Padding(padding: EdgeInsets.all(10)),
-                          CampoForm(
-                            label: 'Email',
-                            controller: _controller1,
-                            isSenha: false,
-                            icone: Icons.email,
-                          ),
-                          const Padding(padding: EdgeInsets.all(20)),
-                          CampoForm(
-                            label: 'Senha',
-                            controller: _controller2,
-                            isSenha: true,
-                            icone: Icons.visibility_off,
-                          ),
-                          const Align(
-                              alignment: Alignment.topLeft,
-                              child: Text(
-                                'Esqueceu a senha?',
-                                style: TextStyle(color: Colors.grey),
-                              )),
-                          const Padding(padding: EdgeInsets.all(20)),
-                          BotaoComponente(
-                            texto: 'Entrar',
-                            corFundo: const Color.fromARGB(255, 12, 175, 158),
-                            onPressed: () {},
-                            corTexto: Colors.white,
-                            corSplash: Colors.white,
-                          ),
-                        ],
->>>>>>> eb23198c28554f028f0cbddddf8ec5957c70c886
                       ),
                     ),
                   ),
                 ),
-<<<<<<< HEAD
                 Padding(
                   padding: const EdgeInsets.only(top: 40),
                   child: Text.rich(TextSpan(
                       text: 'Não tem uma conta? ',
                       children: <InlineSpan>[
                         TextSpan(
-                            text: 'Cadastre-se agora',
-                            style: const TextStyle(
-                                color: Color.fromARGB(255, 12, 175, 158),
-                                decoration: TextDecoration.underline),
-                            recognizer: TapGestureRecognizer()..onTap = () => Navigator.pop(context),
+                          text: 'Cadastre-se agora',
+                          style: const TextStyle(
+                              color: Color.fromARGB(255, 12, 175, 158),
+                              decoration: TextDecoration.underline),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () => Navigator.pop(context),
                         )
                       ])),
                 )
               ],
             ),
-=======
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 40),
-                child: Text.rich(TextSpan(
-                    text: 'Não tem uma conta? ',
-                    children: <InlineSpan>[
-                      TextSpan(
-                          text: 'Cadastre-se agora',
-                          style: const TextStyle(
-                              color: Color.fromARGB(255, 12, 175, 158),
-                              decoration: TextDecoration.underline),
-                          recognizer: TapGestureRecognizer()..onTap = () => Navigator.pop(context),
-                      )
-                    ])),
-              )
-            ],
->>>>>>> eb23198c28554f028f0cbddddf8ec5957c70c886
           ),
         ),
       ),
--- lib/shared/components/campo_form/campo_form_componente.dart
+++ lib/shared/components/campo_form/campo_form_componente.dart
@@ -4,89 +4,54 @@ class CampoForm extends StatefulWidget {
   final TextEditingController controller;
   final String label;
   final IconData icone;
-<<<<<<< HEAD
-  final bool senha;
-
-  const CampoForm({Key? key, required this.controller, required this.label, required this.icone, required this.senha}) : super(key: key);
-=======
   final bool isSenha;
 
-  const CampoForm({Key? key, required this.controller, required this.label, required this.icone, required this.isSenha}) : super(key: key);
->>>>>>> eb23198c28554f028f0cbddddf8ec5957c70c886
+  const CampoForm(
+      {Key? key,
+      required this.controller,
+      required this.label,
+      required this.icone,
+      required this.isSenha})
+      : super(key: key);
 
   @override
   State<CampoForm> createState() => _CampoFormState();
 }
 
 class _CampoFormState extends State<CampoForm> {
-<<<<<<< HEAD
-  bool password = true;
-
-=======
->>>>>>> eb23198c28554f028f0cbddddf8ec5957c70c886
   @override
   Widget build(BuildContext context) {
     return Theme(
-      data: ThemeData(
-        primaryColor: Colors.grey,
-        primarySwatch: Colors.grey
-      ),
+      data: ThemeData(primaryColor: Colors.grey, primarySwatch: Colors.grey),
       child: Column(
         children: [
           Align(
-            alignment: Alignment.bottomLeft,
-            child: Text(widget.label, style: const TextStyle(color: Colors.grey),)),
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                widget.label,
+                style: const TextStyle(color: Colors.grey),
+              )),
           SizedBox(
             height: MediaQuery.of(context).size.height * 0.035,
             child: TextFormField(
               controller: widget.controller,
-<<<<<<< HEAD
-              obscureText: widget.senha == true ?password :false,
-              obscuringCharacter: '●',
-              style: widget.senha ? const TextStyle(fontSize: 20, fontWeight: FontWeight.bold) : null,
-              decoration: InputDecoration(
-                suffixIcon: 
-                widget.senha == true
-                ? Align(
-                  alignment: Alignment.bottomRight,
-                   widthFactor: 1,
-                  heightFactor: 1,
-                  child: IconButton(
-                    onPressed: () {
-                      setState(() {
-                        password = !password;
-                      });
-                    },
-                    icon: password == true
-                        ? const Icon(Icons.visibility_off, color: const Color.fromARGB(255, 12, 175, 158), size: 24,)
-                        : const Icon(Icons.visibility, color: const Color.fromARGB(255, 12, 175, 158), size: 24,),),
-                )
-                      : Align(
-                   alignment: Alignment.bottomRight,
-                   widthFactor: 1,
-                  heightFactor: 1,
-                  child: Icon(widget.icone, color: const Color.fromARGB(255, 12, 175, 158), size: 24,)),
-                isDense: true,
-                contentPadding: const EdgeInsets.fromLTRB(0, 4, 0,0),
-                // Align(
-                //   alignment: Alignment.bottomRight,
-                //   widthFactor: 1,
-                //   heightFactor: 1,
-                //   child: Icon(widget.icone, color: const Color.fromARGB(255, 12, 175, 158), size: 28,))
-=======
               obscureText: widget.isSenha,
               obscuringCharacter: '●',
-              style: widget.isSenha ? const TextStyle(fontSize: 20, fontWeight: FontWeight.bold) : null,
+              style: widget.isSenha
+                  ? const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
+                  : null,
               decoration: InputDecoration(
-                isDense: true,
-                contentPadding: const EdgeInsets.fromLTRB(0, 4, 0,0),
-                suffixIcon: Align(
-                  alignment: Alignment.bottomRight,
-                  widthFactor: 1,
-                  heightFactor: 1,
-                  child: Icon(widget.icone, color: const Color.fromARGB(255, 12, 175, 158), size: 28,))
->>>>>>> eb23198c28554f028f0cbddddf8ec5957c70c886
-              ),
+                  isDense: true,
+                  contentPadding: const EdgeInsets.fromLTRB(0, 4, 0, 0),
+                  suffixIcon: Align(
+                      alignment: Alignment.bottomRight,
+                      widthFactor: 1,
+                      heightFactor: 1,
+                      child: Icon(
+                        widget.icone,
+                        color: const Color.fromARGB(255, 12, 175, 158),
+                        size: 28,
+                      ))),
             ),
           ),
         ],
